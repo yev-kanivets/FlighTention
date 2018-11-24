14f2dcf33ef2a932b9a602f9a7c1ce565689cf6d
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2018-11-24 02:59:53 +0100

    Add other flights

--- app/src/main/java/fr2018/defense/innovation/forum/flightention/repo/FlightRepositoryImpl.kt
+++ app/src/main/java/fr2018/defense/innovation/forum/flightention/repo/FlightRepositoryImpl.kt
@@ -7,10 +7,12 @@ import fr2018.defense.innovation.forum.flightention.model.Flight
 class FlightRepositoryImpl : FlightRepository {
 
     override suspend fun getAllFlights(): LiveData<List<Flight>> {
-        val flightData = Flight("JBU1417", 1543021860, 49.0139128,2.5418305, false, 100.0, 0.0, 10.0, 0)
+        val flight1 = Flight("JBU1417", 1543021860, 49.0139128,2.5418305, false, 100.0, 0.0, 10.0, 0)
+        val flight2 = Flight("JBU1418", 1543021860, 49.0259128,2.5538305, false, 100.0, 0.0, 10.0, 0)
+        val flight3 = Flight("JBU1419", 1543021860, 49.0379128,2.5658305, false, 100.0, 0.0, 10.0, 0)
 
         val liveData = MutableLiveData<List<Flight>>()
-        liveData.value = listOf(flightData)
+        liveData.value = listOf(flight1, flight2, flight3)
 
         return liveData
     }
